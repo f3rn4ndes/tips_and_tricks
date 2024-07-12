cfac6d6289e4c11650ae2dec0fe975bc3fa08837
Jean Fernandes <jean.fernandes.eng@gmail.com>
2024-07-12 10:43:45 -0300

    Initial Commit

--- MockupDeviceSPI/src/main.cpp
+++ MockupDeviceSPI/src/main.cpp
@@ -1,8 +1,18 @@
 #include <Arduino.h>
+#include <SPI.h>
 #include "driver/gpio.h"
 
-const gpio_num_t kDataPin = GPIO_NUM_27;
-const gpio_num_t kClockPin = GPIO_NUM_26;
+const boolean kUsesDefaultSpiPins = false;
+
+const boolean kUsesSpiClockDivider = false;
+
+const uint32_t kSpiFrequency = 1536000;
+
+const uint8_t kSpiSckPin = GPIO_NUM_27;
+const uint8_t kSpiMosiPin = GPIO_NUM_26;
+const uint8_t kSpiMisoPin = GPIO_NUM_33;
+const uint8_t kSpiCsPin = GPIO_NUM_25;
+
 const gpio_num_t kProcessPin = GPIO_NUM_2;
 
 const uint32_t kSerialBaudRate = 115200;
@@ -12,14 +22,13 @@ const uint32_t kDelayLoopMs = 1000;
 const uint32_t kDelayBetweenFramesUs = 125;
 
 const uint32_t kChannelsSize = 8;
-
-const uint32_t kFrameSizeBits = 16;
-
 const uint16_t kFrameDataMask = 0x0FFF;
 
-const uint32_t kDelayBitNs = 300;
+// globals
+SPISettings spiSettings(kSpiFrequency, MSBFIRST, SPI_MODE0);
 
-const uint32_t kDelayRisingClockLine = 1;
+void SpiSetup(uint8_t spi_mosi_pin, uint8_t spi_miso_pin,
+              uint8_t spi_sck_pin, uint8_t spi_cs_pin);
 
 void IRAM_ATTR SetPinLevel(uint32_t pin, boolean level);
 
@@ -31,11 +40,10 @@ void setup()
 {
   delay(kDelayStartMs);
   Serial.begin(kSerialBaudRate);
+
+  SpiSetup(kSpiMosiPin, kSpiMisoPin, kSpiSckPin, kSpiCsPin);
+
   // Set the pins as outputs
-  gpio_pad_select_gpio(kDataPin);
-  gpio_set_direction(kDataPin, GPIO_MODE_OUTPUT);
-  gpio_pad_select_gpio(kClockPin);
-  gpio_set_direction(kClockPin, GPIO_MODE_OUTPUT);
   gpio_pad_select_gpio(kProcessPin);
   gpio_set_direction(kProcessPin, GPIO_MODE_OUTPUT);
 
@@ -43,9 +51,6 @@ void setup()
 
   Serial.printf("\n----------------------------------\n");
   Serial.printf("Start Application\n\n");
-
-  SetPinLevel(kClockPin, false);
-  SetPinLevel(kClockPin, false);
 }
 
 void loop()
@@ -54,12 +59,13 @@ void loop()
 
   // Test by writing values 0 to 15
   Serial.printf("Start to Generate Data\n");
+
   SetPinLevel(kProcessPin, false);
   for (uint16_t i = 0; i < kChannelsSize; i++)
   {
     WriteChannel(i, channel_value);
-    delayMicroseconds(kDelayBetweenFramesUs);
-    // channel_value ^= 0xFFFF;
+    if (i < (kChannelsSize - 1))
+      delayMicroseconds(kDelayBetweenFramesUs);
   }
   SetPinLevel(kProcessPin, true);
   delay(kDelayLoopMs);
@@ -69,30 +75,25 @@ void IRAM_ATTR WriteChannel(uint16_t address, uint16_t value)
 {
   uint16_t tx_frame = (value & kFrameDataMask) | (address << 12);
 
-  // Serial.printf("Address: %04X, Value: %04X, Frame: %04X\n", address, value, tx_frame);
-  // delay(kDelayLoopMs);
-
-  // Serial.printf("Frame: ");
-  for (uint32_t i = 0; i < kFrameSizeBits; i++)
-  {
-    // uint8_t bit_value = (tx_frame & 0x8000) > 0 ? 1 : 0;
-    // Serial.printf("%d", bit_value);
-    SetPinLevel(kDataPin, (tx_frame & 0x8000) > 0 ? true : false);
-    tx_frame <<= 1;
-    // DelayNanoSeconds(kDelayRisingClockLine);
-    SetPinLevel(kClockPin, true);
-    DelayNanoSeconds(kDelayBitNs);
-    SetPinLevel(kClockPin, false);
-    DelayNanoSeconds(kDelayBitNs - kDelayRisingClockLine);
-  }
-  // Serial.printf("\n");
-  // delay(kDelayLoopMs);
+  SPI.beginTransaction(spiSettings);
+  SPI.transfer16(tx_frame);
+  SPI.endTransaction();
 }
 
-void IRAM_ATTR DelayNanoSeconds(uint32_t value)
+void SpiSetup(uint8_t spi_mosi_pin, uint8_t spi_miso_pin, uint8_t spi_sck_pin, uint8_t spi_cs_pin)
 {
-  for (uint32_t i = 0; i < value; i++)
-    ;
+  if (kUsesDefaultSpiPins)
+    SPI.begin();
+  else
+    SPI.begin(spi_sck_pin, spi_miso_pin, spi_mosi_pin, spi_cs_pin);
+
+  if (kUsesSpiClockDivider)
+  {
+    uint32_t clockDiv = (80000000 + (kSpiFrequency - 1)) / kSpiFrequency; // Calculate the clock divider for 1.536 MHz
+
+    // Set clock divisor
+    SPI.setClockDivider((clockDiv << 12) | (clockDiv << 6) | (clockDiv));
+  }
 }
 
 // Helper function to set a GPIO pin to a specific level
